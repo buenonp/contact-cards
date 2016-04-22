35995f22231aa9cddd048cdae17b4ba40a8cb959
Bueno Nepomuceno <buenonp@gmail.com>
2016-04-22 13:28:59 -0300

    Initial commit

---
+++ js/index.js
@@ -0,0 +1,33 @@
+(function () {
+
+  'use strict';
+
+  angular
+      .module('ContactCards',['ngMaterial'])
+      .controller('ContactCardCtrl', function () {
+        
+        var contactList = this;
+        
+        contactList.contacts = [];
+
+        contactList.addContact = function() {
+          
+          contactList.contacts.push(
+
+            {
+              name: contactList.name,
+              email: contactList.email,
+              phone: contactList.phone,
+              image: 'http://lorempixel.com/80/80/people/' + contactList.contacts.length
+            }
+
+          );
+          
+          //contactList.name = '';
+          //contactList.email = '';
+          //contactList.phone = '';
+
+        };
+
+      });
+})();
